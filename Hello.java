dd93672952676c30fa1d69d18921ae970c8d0da7
mldn <mldnqa@163.com>
2016-11-04 17:21:29 +0800

    changge hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 public class Hello{
 	public static void main(String[] args) {
-System.out.print("ssssssssssss");
-		System.out.print("nulinuli");
-
-		Dept dept=new Dept();
-	}
+System.out.print("创建补丁");
+System.out.print("多运动好好学习");
+}
 }
