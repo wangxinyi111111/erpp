9318ef720bf3743443c120e4f22acf3e28c4f930
mldn <mldnqa@163.com>
2016-11-04 15:16:14 +0800

    change

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	public static void main(String[] args) {
 System.out.print("ssssssssssss");
 		System.out.print("nulinuli");
-		Admin admin=new Admin();
+
 		Dept dept=new Dept();
 	}
 }
